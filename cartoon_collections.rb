5853b76a0e64938ec5b5c277fa23008396e5ed72
Jaden Locke <jadenlocke1113@gmail.com>
2020-07-07 19:32:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   hash = Hash.new
 array.each_with_index do |item, index|
-puts "#{index + 1}. #{item}
+puts "#{index + 1}. #{item}"
 # dwarves = ["Dopey", "Grumpy", "Bashful"]
   end
 end
